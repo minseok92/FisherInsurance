ba5071fee0578073adcec53eb25d2e85c5575940
minseok92 <kim.3926@osu.edu>
2017-02-01 22:26:29 -0500

    First commit

---
+++ Controllers/CustomerCareController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+[Route("customer")]
+public class CustomerCareController : Controller
+{
+    public IActionResult Index()
+    {
+        return Ok("This is the index of the CustomerCareController");
+    }
+
+    [Route("customer/claim")]
+    public IActionResult Quote()
+    {
+        return Ok("This is the claims of the CustomerCareController");
+    }
+
+    [Route("claim/fileclaim")]
+    public IActionResult NewClaim()
+    {
+        return Ok("This is the fileclaims of the CustomerCareController");
+    }
+
+    [Route("claim/myclaims")]
+    public IActionResult ClaimHistory()
+    {
+        return Ok("This is the myclaims of the CustomerCareController");
+    }
+}

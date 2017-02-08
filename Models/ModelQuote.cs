cf3390120be4a003875c4921ddb94d5b9ab37ddf
minseok92 <kim.3926@osu.edu>
2017-02-08 17:58:45 -0500

    Add changes

---
+++ Models/ModelQuote.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace FisherInsurance.Models
+
+{
+    public class Quote
+    
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Price { get; set; }
+
+    }
+
+}
